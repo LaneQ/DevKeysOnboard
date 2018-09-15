644c5a22f5d9575a2ad6d14b909a22e9812076a2
Lei <853271265@qq.com>
2018-09-15 20:49:20 +1200

    Initial commit

---
+++ DevKeysOnboarding/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevKeysOnboarding.Startup))]
+namespace DevKeysOnboarding
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
